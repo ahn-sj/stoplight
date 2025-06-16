818140f7098cd1163fbdc0772444f8b3b35c8eb7
ahn-sj <jaesa5221@gmail.com>
2025-06-16 15:11:12 +0900

    Initial Project

---
+++ backend/src/main/java/tally/stoplight/StoplightApplication.java
@@ -0,0 +1,13 @@
+package tally.stoplight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoplightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoplightApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/tally/stoplight/StoplightApplicationTests.java
@@ -0,0 +1,13 @@
+package tally.stoplight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoplightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
